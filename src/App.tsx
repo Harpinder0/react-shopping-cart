3ca9820f05cf0eebffff5478d874ce94961b387c
Harpinder Singh <Harpinderbhullar84@gmail.com>
2021-09-10 23:04:31 +0530

    remove button work

--- src/App.tsx
+++ src/App.tsx
@@ -59,21 +59,21 @@ const App = () => {
     });
   };
 
-  const handleRemoveFromCart = () => null;  
-  //   setCartItems(
-  //     prev => (
-  //     prev.reduce((ack, item) => {
-  //       if (item.id === id) {
-  //         if (item.amount === 1) return ack;
-  //         return [...ack, { ...item, amount: item.amount - 1 }];
-  //       } else {
-  //         return [...ack, item];
-  //       }
-  //     }),
-  //     [] as CartItemType[]
-  //   )
-  // );
-  // };
+  const handleRemoveFromCart = (id: number) => {  
+    setCartItems(
+      prev => (
+      prev.reduce((ack, item) => {
+        if (item.id === id) {
+          if (item.amount === 1) return ack;
+          return [...ack, { ...item, amount: item.amount - 1 }];
+        } else {
+          return [...ack, item];
+        }
+      },  [] as CartItemType[])
+     
+    )
+  );
+  };
 
   if (isLoading) return <LinearProgress />;
   if (error) return <div>Something went wrong ...</div>;
